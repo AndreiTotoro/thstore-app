27b3a46c53e26c2a728ee7cae8df2903487c717b
AndreiTotoro <AndreiTotoro@gmail.com>
2022-10-22 13:37:30 +0300

    Style the search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
-import { Input } from '@chakra-ui/react';
+import { Box, Input } from '@chakra-ui/react';
 import React from 'react';
-
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
 function Search({ search, updateSearch, setShowAll }) {
   const handleSearch = e => {
     updateSearch(e.target.value);
@@ -11,19 +11,28 @@ function Search({ search, updateSearch, setShowAll }) {
     }
   };
   return (
-    <Input
+    <Box
       boxShadow="xs"
       rounded="xl"
+      p={2}
       paddingTop={1}
       paddingBottom={1}
       borderColor="#171624"
       background="#171624"
-      value={search}
-      onChange={handleSearch}
-      placeholder="Here is a sample placeholder"
-      size="sm"
-      color="white"
-    />
+      display="flex"
+      alignContent="center"
+      alignItems="center"
+    >
+      <TravelExploreIcon sx={{ color: '#646371' }} />
+      <Input
+        focusBorderColor="0"
+        value={search}
+        onChange={handleSearch}
+        placeholder="Here is a sample placeholder"
+        size="sm"
+        color="#646371"
+      />
+    </Box>
   );
 }
 

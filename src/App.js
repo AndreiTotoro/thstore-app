eca95477bb5ad18c0272ed2b2077323f413c5122
AndreiTotoro <AndreiTotoro@gmail.com>
2022-10-22 08:48:08 +0300

    Add cardcreator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, ChakraProvider } from '@chakra-ui/react';
 
 function App() {
+  const [cards, addCard] = useState([]);
+
+  const cardCreator = (name, img, author, price) => {
+    const new_card = {
+      name: name,
+      img: img,
+      author: author,
+      price: price,
+    };
+    return new_card;
+  };
+
+  useEffect(() => {
+    addCard([...cards, cardCreator('John', 'someimg.jpg', 'Andrei', '300')]);
+    addCard([...cards, cardCreator('Udad', 'someimg.jpg', 'Andrei', '30dsa0')]);
+    addCard([
+      ...cards,
+      cardCreator('saad', 'someimdsg.jpg', 'Andrei', '30dsa0'),
+    ]);
+  }, []);
+
   return (
     <ChakraProvider>
       <Box bg="tomato" w="100%" p={4} color="white">
